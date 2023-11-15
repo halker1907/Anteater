4b1ab35ddebe66db95d1774f699b1568214edf5a
halker1907 <113712157+halker1907@users.noreply.github.com>
2023-11-15 19:46:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 class Cell:
-    def __init__(self, image='☐', Y=None, X=None):
+    '''клетка игрового поля'''
+    def __init__(self, image='.', Y=None, X=None):
+        self.image = image
+        self.Y = Y
+        self.X = X
+        self.content = None
+
+class Player:
+    ''' игрок '''
+    def __init__(self, image='p', Y=None, X=None):
         self.image = image
         self.Y = Y
         self.X = X
 
 class Field:
-    def __init__(self, rows=10, cols=25, cell=Cell):
-        self.rows = rows
-        self.cols = cols
-        self.cells = [[cell(Y=y, X=x) for x in range(cols)] for y in range(rows)]
+    def __init__(self, ROWS=10, COLS=25, cell=Cell, palyer=Player):
+        self.rows = ROWS
+        self.cols = COLS
+        self.cells = self.make_cells()
+        self.player = Player()
 
-    def drawrows(self):
+    def draw_cells(self):
+        """выводит игровое поле на экран"""
+        #TODO вывести игрока на поле
         for row in self.cells:
-            print(*[cell.image for cell in row])
+            for col in row:
+                print(col.image, end='')
+            print()
+            '''
+        for row in self.player:
+            for gr in row:
+                print(gr.image)
+            print()
+            '''
+    
+    def make_cells(self, ROWS=10, COLS=25, cell=Cell):
+        cells = [[cell(Y=y, X=x) for x in range(COLS)] for y in range(ROWS)]
+        return cells
+    
 
-MyClass = Field()
-MyClass.drawrows()
-
-
+MCs = Field()
+MCs.draw_cells()
