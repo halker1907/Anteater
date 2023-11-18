19a3d566703e2e2fe56d5aa294b4323e7e9f1acd
halker1907 <113712157+halker1907@users.noreply.github.com>
2023-11-18 11:42:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Cell:
     '''клетка игрового поля'''
     def __init__(self, image='.', Y=None, X=None):
@@ -14,25 +16,23 @@ class Player:
         self.X = X
 
 class Field:
-    def __init__(self, ROWS=10, COLS=25, cell=Cell, palyer=Player):
+    def __init__(self, ROWS=10, COLS=25, player=Player):
         self.rows = ROWS
         self.cols = COLS
         self.cells = self.make_cells()
-        self.player = Player()
+        self.player = player(Y=random.randint(0, ROWS-1), X=random.randint(0, COLS-1))
+        self.cells[self.player.Y][self.player.X].content = self.player
 
     def draw_cells(self):
         """выводит игровое поле на экран"""
-        #TODO вывести игрока на поле
         for row in self.cells:
-            for col in row:
-                print(col.image, end='')
+            for cell in row:
+                if cell.content is not None:
+                    print(cell.content.image, end=' ')
+                else:
+                    print(cell.image, end=' ')
             print()
-            '''
-        for row in self.player:
-            for gr in row:
-                print(gr.image)
-            print()
-            '''
+            
     
     def make_cells(self, ROWS=10, COLS=25, cell=Cell):
         cells = [[cell(Y=y, X=x) for x in range(COLS)] for y in range(ROWS)]
